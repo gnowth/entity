5c1b2dc0012514c9b42afaa67bfd85615ceab843
Thierry Wong <twong@192-168-1-5.tpgi.com.au>
2018-09-23 17:31:53 +1000

    temp commit

---
+++ packages/ui/src/index.js
@@ -0,0 +1,15 @@
+export { default as Button } from './Button';
+
+export { default as Icon } from './Icon';
+
+export { default as Label } from './Label';
+
+export { default as Portal } from './Portal';
+
+export { default as ProgressCircle } from './ProgressCircle';
+
+export { default as ProgressLine } from './ProgressLine';
+
+export { default as Spacer } from './Spacer';
+
+export { default as TypeSet } from './TypeSet';
