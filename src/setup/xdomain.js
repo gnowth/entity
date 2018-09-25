fd89ad0bf436fb010f297beb3e5cbe6eb4bce523
Thierry Wong <twong@192-168-1-6.tpgi.com.au>
2018-09-26 08:39:22 +1000

    updated setup xdomain

--- src/setup/xdomain.js
+++ src/setup/xdomain.js
@@ -1,7 +1,7 @@
 import { xdomain } from 'xdomain';
 
 export default function setupXDomain(settings) {
-  xdomain.slaves({
+  return xdomain.slaves({
     [settings.BASE_API_DOMAIN]: '/api/xdomain/v1/proxy',
   });
 }
