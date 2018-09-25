1f197c2ecf5ae30840dca0e3284c43b4a41e7b91
Thierry Wong <twong@192-168-1-6.tpgi.com.au>
2018-09-25 18:40:39 +1000

    used injectGlobal from emotion

--- packages/react/lib/app-setup/styled-components-global.js
+++ packages/react/lib/app-setup/styled-components-global.js
@@ -1,4 +1,4 @@
-import { injectGlobal } from 'styled-components';
+import { injectGlobal } from 'emotion';
 
 import { global } from 'styles';
 
--- packages/react/src/apps/snippet/forms/Main/styles.js
+++ packages/react/src/apps/snippet/forms/Main/styles.js
@@ -1,4 +1,4 @@
-import { css } from 'react-emotion';
+import { css } from 'emotion';
 
 export const inputs = css`
   margin-top: 20px;
