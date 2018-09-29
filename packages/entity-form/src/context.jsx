de2a1128d4458ca0095b1c65762674daf4a3c446
Thierry Wong <twong@192-168-1-6.tpgi.com.au>
2018-09-30 09:34:16 +1000

    remove form default

--- packages/entity-form/src/context.jsx
+++ packages/entity-form/src/context.jsx
@@ -7,11 +7,6 @@ import React from 'react';
 
 const { Provider, Consumer } = React.createContext({});
 
-const { Provider: DefaultProvider, Consumer: DefaultConsumer } = React.createContext({
-  defaultComponents: {},
-  defaultWidgets: {},
-});
-
 
 // TODO:REMOVE if not being used
 export const withForm = ComposedComponent => function withFormConsumer(props) {
@@ -56,31 +51,4 @@ FormProvider.defaultProps = {
   formName: undefined,
   formNameMapper: {},
   formReadOnly: false,
-};
-
-
-export const FormDefaultConsumer = DefaultConsumer;
-
-
-// TODO:OPTIMISE? props is being generated and it will rerender all the time when children changes
-export const FormDefaultProvider = ({ children, ...props }) => (
-  <DefaultProvider value={props}>
-    { children }
-  </DefaultProvider>
-);
-
-FormDefaultProvider.propTypes = exact({
-  children: PropTypes.node.isRequired,
-  defaultComponents: PropTypes.shape({
-    control: PropTypesPlus.component,
-    error: PropTypesPlus.component,
-    label: PropTypesPlus.component,
-    query: PropTypesPlus.component,
-  }),
-  defaultWidgets: PropTypes.objectOf(PropTypesPlus.component.isRequired),
-});
-
-FormDefaultProvider.defaultProps = {
-  defaultComponents: {},
-  defaultWidgets: {},
 };
