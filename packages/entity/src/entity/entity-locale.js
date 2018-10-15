17b8f7d3d1dbec50b61d3017a4b07d98cc228f10
Thierry Wong <twong@192-168-1-9.tpgi.com.au>
2018-10-16 09:11:10 +1100

    temp commit [skip ci]

--- packages/entity/src/entity/entity-locale.js
+++ packages/entity/src/entity/entity-locale.js
@@ -1,10 +1,13 @@
 import Entity from './entity';
+import AnyField from '../field/field-any';
 import CharField from '../field/field-char';
 import IdField from '../field/field-id';
 
 export default class Locale extends Entity {
   static fields = {
     // TODO add translations here. in same format that we pass to intlProvider
+    // TODO check if AnyField is appropriate for messages
+    messages: new AnyField(), // store all messages in a map/object with key as the language
     defaultMessage: new CharField(),
     description: new CharField(),
     id: new CharField(),
