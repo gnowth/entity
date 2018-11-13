c95d31547889520d8a31135c729925d53f017efc
Thierry Wong <twong@192-168-1-9.tpgi.com.au>
2018-11-13 19:22:15 +1100

    feat: added UIImage

--- packages/ui/src/stories.jsx
+++ packages/ui/src/stories.jsx
@@ -1,4 +1,5 @@
 import uiButton from './Button/stories';
+import uiImage from './Image/stories';
 import uiProcessingCircle from './ProgressCircle/stories';
 import uiProcessingLine from './ProgressLine/stories';
 import uiTypeSet from './TypeSet/stories';
@@ -6,6 +7,7 @@ import uiWell from './Well/stories';
 
 export default (mod) => {
   uiButton(mod);
+  uiImage(mod);
   uiProcessingCircle(mod);
   uiProcessingLine(mod);
   uiTypeSet(mod);
