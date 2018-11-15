54d74d98052d489178b8e9f54d6df4bca8520a8e
Thierry Wong <twong@192-168-1-9.tpgi.com.au>
2018-11-15 23:43:04 +1100

    temp commit [skip ci]

--- packages/entity/src/validator/entity.js
+++ packages/entity/src/validator/entity.js
@@ -2,7 +2,7 @@ import _isFunction from 'lodash/isFunction';
 
 export default validatorOptions => (value, options) => {
   if (process.env.NODE_ENV !== 'production') {
-    if (!options.field) throw new Error('entity-validator (entity): "field" option is required');
+    if (!options.field) throw new Error('validator.validatorOptions: "field" option is required');
   }
 
   return _isFunction(validatorOptions)
