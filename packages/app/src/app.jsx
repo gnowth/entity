9e4cf24c51bc94012ad3eeb23d81104a3d99dcbb
Thierry Wong <gnowth@users.noreply.github.com>
2018-11-15 18:47:13 +1100

    feat: added a containerComponent for app (#18)

--- packages/app/src/app.jsx
+++ packages/app/src/app.jsx
@@ -1,6 +1,7 @@
 import _isFunction from 'lodash/isFunction';
 import exact from 'prop-types-exact';
 import PropTypes from 'prop-types';
+import PropTypesPlus from '@gnowth/prop-types-plus';
 import React from 'react';
 import { DefaultProvider } from '@gnowth/default';
 
@@ -16,6 +17,7 @@ const App = props => (
 
       const IntlProvider = (props.intlProviderProps && context.intlProvider) || React.Fragment;
       const ThemeProvider = (props.themeProviderProps && context.themeProvider) || React.Fragment;
+      const Container = props.containerComponent || React.Fragment;
 
       const getProps = name => (
         _isFunction(props[name])
@@ -27,7 +29,9 @@ const App = props => (
         <DefaultProvider {...Object.assign({}, context.defaults, props.defaults)}>
           <IntlProvider {...getProps('intlProviderProps')}>
             <ThemeProvider {...getProps('themeProviderProps')}>
-              { props.children }
+              <Container {...(props.containerComponentProps || {})}>
+                { props.children }
+              </Container>
             </ThemeProvider>
           </IntlProvider>
         </DefaultProvider>
@@ -38,20 +42,21 @@ const App = props => (
 
 App.propTypes = exact({
   children: PropTypes.node.isRequired,
-
+  containerComponent: PropTypesPlus.isRequiredIf('containerComponentProps', PropTypesPlus.component),
+  containerComponentProps: PropTypes.shape({}),
   defaults: PropTypes.shape({}),
-
   intlProviderProps: PropTypes.exact({
     locale: PropTypes.string.isRequired,
     messages: PropTypes.object.isRequired,
   }),
-
   themeProviderProps: PropTypes.exact({
     theme: PropTypes.shape({}).isRequired,
   }),
 });
 
 App.defaultProps = {
+  containerComponent: undefined,
+  containerComponentProps: undefined,
   defaults: undefined,
   intlProviderProps: undefined,
   themeProviderProps: undefined,
