15df6c46480aa3f8149d9107867494d24b1c1448
Thierry Wong <gnowth@users.noreply.github.com>
2018-11-21 18:30:36 +1100

    feat (view): added ViewTypeSet (#37)

--- packages/entity-view/src/index.js
+++ packages/entity-view/src/index.js
@@ -1,5 +1,7 @@
 export { default as ViewLink } from './Link';
 
+export { default as ViewRedirectOnCreate } from './RedirectOnCreate';
+
 export { default as ViewScreen } from './Screen';
 
-export { default as ViewRedirectOnCreate } from './RedirectOnCreate';
+export { default as ViewTypeSet } from './TypeSet';
