0c503d97508d1da7d33f4d26386541e853461ad1
Thierry Wong <@circleci.com>
2019-02-28 20:14:43 +1100

    refactor: apply decorators at story level

--- packages/dev-client/.storybook/config.js
+++ packages/dev-client/.storybook/config.js
@@ -4,9 +4,51 @@ import 'font-awesome/css/font-awesome.css';
 import 'material-design-icons/iconfont/material-icons.css';
 import 'normalize.css/normalize.css';
 
-import { configure } from '@storybook/react';
+import React from 'react';
+import { defaultTheme, GlobalStyles } from '@gnowth/style';
+import { withNotes } from '@storybook/addon-notes';
+import { withOptions } from '@storybook/addon-options';
+import { addDecorator, configure } from '@storybook/react';
+import { ConnectedRouter } from 'connected-react-router/immutable';
+import { Provider } from 'react-redux';
+import { IntlProvider } from 'react-intl';
+import { ThemeProvider } from 'styled-components';
+
+import * as theme from 'styles';
+import store, { history } from 'store';
 
 const componentReq = require.context('src', true, /stories\.jsx$/);
+const themeProviderProps = { theme: { ...defaultTheme, ...theme } };
+
+addDecorator(withNotes);
+
+addDecorator(withOptions({
+  sortStoriesByKind: true,
+}));
+
+addDecorator(story => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      { story() }
+    </ConnectedRouter>
+  </Provider>
+));
+
+addDecorator(story => (
+  <IntlProvider locale="en" messages={{}}>
+    { story() }
+  </IntlProvider>
+));
+
+addDecorator(story => (
+  <ThemeProvider {...themeProviderProps}>
+    <>
+      { story() }
+
+      <GlobalStyles />
+    </>
+  </ThemeProvider>
+));
 
 configure(() => {
   componentReq.keys().forEach(filename => componentReq(filename));
--- packages/dev-client/src/stories.jsx
+++ packages/dev-client/src/stories.jsx
@@ -1,41 +1,15 @@
 import centered from '@storybook/addon-centered';
-import React from 'react';
-import { createStoriesForTheme, defaultTheme, GlobalStyles } from '@gnowth/style';
+import { createStoriesForTheme } from '@gnowth/style';
 import { createStoriesForUI } from '@gnowth/ui';
 import { createStoriesForWidget } from '@entity/widget';
 import { checkA11y } from '@storybook/addon-a11y';
 import { withKnobs } from '@storybook/addon-knobs';
-import { withNotes } from '@storybook/addon-notes';
-import { withOptions } from '@storybook/addon-options';
 import { storiesOf } from '@storybook/react';
-import { IntlProvider } from 'react-intl';
-import { ThemeProvider } from 'styled-components';
-
-import * as theme from 'styles';
-
-const computedTheme = { ...defaultTheme, ...theme };
 
 const storyFactory = story => chapter => storiesOf(`${story}/${chapter}`, module)
   .addDecorator(centered)
   .addDecorator(checkA11y)
-  .addDecorator(withNotes)
-  .addDecorator(withOptions({ sortStoriesByKind: true }))
-  .addDecorator(withKnobs)
-  .addDecorator(storyFn => (
-    <IntlProvider locale="en" messages={{}}>
-      { storyFn() }
-    </IntlProvider>
-  ))
-  .addDecorator(storyFn => (
-    <ThemeProvider theme={computedTheme}>
-      <>
-        { storyFn() }
-
-        <GlobalStyles />
-      </>
-    </ThemeProvider>
-  ));
-
+  .addDecorator(withKnobs);
 
 createStoriesForTheme(storyFactory('Theme'));
 createStoriesForUI(storyFactory('UI'));
