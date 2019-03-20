414b7e44c4993b886515727dabaec3ea0bcb99b9
Thierry Wong <@circleci.com>
2019-03-21 00:17:45 +1100

    fix: build for non changing root

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,30 +10,11 @@ current=$(git symbolic-ref --short HEAD)
 git config --global user.email "$CIRCLE_USERNAME@circleci.com"
 git config --global user.name "$CIRCLE_USERNAME"
 
-# copy package.json to packages/root to get updated by lerna
-mkdir -p packages/root
-cp package.json packages/root
-
 lerna version --exact --amend --convertional-commits --yes --no-commit-hooks --no-git-tag-version --no-push
 
-# get updated package.json from packages/root
-cp -f packages/root/package.json .
-rm -r packages/root
-
 # Commiting changes
-version="v$(npx -c 'echo "$npm_package_version"')"
-
-# publishing
-npm run packages-build
-git add -A
-git commit --amend -m "$version [skip ci]"
-git tag -a $version -m $version
-
-lerna publish from-git --yes
-
-npm run packages-link-main:src
-npm run packages-link-module:src
-git tag -d $version
+version="v$(npx -c 'json -f lerna.json version')"
+npx -c 'json -I -f package.json -e "this.version=\"$version\""'
 
 # Updating branch
 git add -A
@@ -41,5 +22,9 @@ git commit --amend -m "$version [skip ci]"
 git tag -a $version -m $version
 git push origin $version
 git push origin $current --force
+
+# publishing
+npm run packages-build
+lerna publish from-git --yes
 
 echo "Finished Branch Update!"
