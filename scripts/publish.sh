5d7bf5bfa2ba389734cc28ff50cd39e0e8a79376
Thierry Wong <gnowth@users.noreply.github.com>
2019-03-21 01:20:46 +1100

    fix: build not publishing (#147)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -16,17 +16,23 @@ lerna version --exact --amend --convertional-commits --yes --no-commit-hooks --n
 version="v$(npx -c 'json -f lerna.json version')"
 npx -c 'json -I -f package.json -e "this.version=\"$version\""'
 
+# publishing
+npm run packages-build
+git add -A
+git commit --amend -m "$version [skip ci]"
+git tag -a $version -m $version
+
+lerna publish from-git --yes
+
+npm run packages-link-main:src
+npm run packages-link-module:src
+git tag -d $version
+
 # Updating branch
 git add -A
 git commit --amend -m "$version [skip ci]"
 git tag -a $version -m $version
 git push origin $version
 git push origin $current --force
-
-# publishing
-npm run packages-build
-git add -A
-git commit --amend -m "$version [skip ci]"
-lerna publish from-git --yes
 
 echo "Finished Branch Update!"
