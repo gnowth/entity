530f9b451079b1f92cee83185dd10cbb8d195cb8
Thierry Wong <@circleci.com>
2019-03-31 10:50:14 +1100

    extract theme from index

--- packages/theme-standard/src/index.js
+++ packages/theme-standard/src/index.js
@@ -2,38 +2,12 @@ import { getPalettes } from '@gnowth/theme';
 
 import palettes from './palettes.json';
 
-import * as boxshadows from './boxshadows';
-import * as scales from './scales';
-import * as vars from './vars';
-
-import * as uiButton from './components/UIButton';
-import * as uiProgressCircle from './components/UIProgressCircle';
-import * as uiType from './components/UIType';
-import * as uiCard from './components/UICard';
-import * as viewLink from './components/ViewLink';
-import * as widgetCheckbox from './components/WidgetCheckbox';
-import * as widgetDate from './components/WidgetDate';
-import * as widgetInput from './components/WidgetInput';
-import * as widgetPagination from './components/WidgetPagination';
-import * as widgetTextarea from './components/WidgetTextarea';
+import * as theme from './theme';
 import global from './global';
 
 export default {
+  ...theme,
   global,
 
   ...getPalettes(palettes),
-  ...boxshadows,
-  ...scales,
-  ...vars,
-
-  ...uiButton,
-  ...uiCard,
-  ...uiProgressCircle,
-  ...uiType,
-  ...viewLink,
-  ...widgetCheckbox,
-  ...widgetDate,
-  ...widgetInput,
-  ...widgetPagination,
-  ...widgetTextarea,
 };
